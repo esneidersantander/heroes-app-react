5763e9e7a45e4a35b6139be0bf109704cf8e3354
LimberthEsneiderSantanderAlcivar <esneider_98@outlook.com>
2021-01-26 20:03:36 -0500

    navegando con dos routers, se puede agregar como componente un conjunto de ruta que son funcional component

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
-export const LoginScreen = () => {
+export const LoginScreen = ({history}) => {
+    const handleLogin=()=>{
+        //redirige hacia una direccion dada pero sse puede regresar al login
+        //Inserta una nueva entrada en la pila del historial
+        history.push('/');
+        //al redirigir Reemplaza la entrada actual en la pila del historial 
+        history.replace('/');
+    }
+
+
     return (
-        <div>
+        <div className="container mt-5">
             <h1>Login</h1>
+                <hr/>
+                <button
+                    className="btn btn-primary"
+                    onClick={handleLogin}
+                >
+                Login    
+            </button>
         </div>
     )
 }
