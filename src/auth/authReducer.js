7400dbaa18feccfdc5b3528efe0f25b97b73be0b
LimberthEsneiderSantanderAlcivar <esneider_98@outlook.com>
2021-01-28 12:00:09 -0500

    login y logout de la app (osea no son reales pero serviran para entender lo de rutas privdas y publicas)

--- src/auth/authReducer.js
+++ src/auth/authReducer.js
@@ -5,7 +5,7 @@ export const authReducer = (state ={}, action)=>{
     switch (action.type) {
         case types.login:
             return {
-                ...action.pyaload,
+                ...action.payload,
                 logged: true
             }            
         case types.logout:
--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../auth/AuthContext'
+import { types } from '../../types/types'
 
 export const LoginScreen = ({history}) => {
+    const { dispatch} = useContext(AuthContext);
     const handleLogin=()=>{
         //redirige hacia una direccion dada pero sse puede regresar al login
         //Inserta una nueva entrada en la pila del historial
         //history.push('/');
         //al redirigir Reemplaza la entrada actual en la pila del historial 
+        //history.replace('/');
+        const newUser ={
+            name: 'Limbert'
+        }
+        const action ={
+            type: types.login,
+            payload:newUser
+        }
+
+        dispatch(action);
+
         history.replace('/');
-    }
+    }   
 
 
     return (
